e54002c6842a261b2a9c4107e115880ec6f6d36d
gurov-e <pistoleta226@yandex.ru>
2022-05-21 21:46:24 +0300

    update

--- script.js
+++ script.js
@@ -3,29 +3,55 @@ const controls = document.querySelectorAll('.control')
 let activeSlideIdx = 0
 
 function clear() {
-    slides.forEach(img => img.classList.remove('show'))
-    controls.forEach(item => item.classList.remove('active-btn'))
+  slides.forEach(img => img.classList.remove('show'))
+  controls.forEach(item => item.classList.remove('active-btn'))
 }
 
 setInterval(() => {
-    clear()
+  clear()
 
-    if (activeSlideIdx < slides.length) {
-        activeSlideIdx++
-        if (activeSlideIdx === slides.length) {
-            activeSlideIdx = 0
-        }
-        slides[activeSlideIdx].classList.add('show')
-        controls[activeSlideIdx].classList.add('active-btn')
+  if (activeSlideIdx < slides.length) {
+    activeSlideIdx++
+    if (activeSlideIdx === slides.length) {
+      activeSlideIdx = 0
     }
+    slides[activeSlideIdx].classList.add('show')
+    controls[activeSlideIdx].classList.add('active-btn')
+  }
 }, 5000)
 
 controls.forEach((item, idx) => {
-    item.addEventListener('click', () => {
-        clear()
-    
-        item.classList.add('active-btn')
-        slides[idx].classList.add('show')
-        activeSlideIdx = idx
-    })
+  item.addEventListener('click', () => {
+    clear()
+
+    item.classList.add('active-btn')
+    slides[idx].classList.add('show')
+    activeSlideIdx = idx
+  })
+
+})
+
+window.addEventListener('keydown', (e) => {
+  if (e.code === 'ArrowRight' || e.code === 'ArrowLeft') {
+    clear()
+
+    if (e.code === 'ArrowRight') {
+      activeSlideIdx++
+
+      if (activeSlideIdx === slides.length) {
+        activeSlideIdx = 0
+      }
+    }
+
+    if (e.code === 'ArrowLeft') {
+      activeSlideIdx--
+
+      if (activeSlideIdx < 0) {
+        activeSlideIdx = slides.length - 1
+      }
+    }
+
+    slides[activeSlideIdx].classList.add('show')
+    controls[activeSlideIdx].classList.add('active-btn')
+  }
 })
